19eb42fae1980e419df2406cb01c3eb0bc4536df
Administrator <Administrator@DESKTOP-6TPD0OM>
2021-02-01 15:33:04 +0800

    初始化项目(备注)

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, {
+    Fragment
+} from 'react';
+
+function App() {
+  return (
+      <Fragment>
+          <div className="App">
+              <h1>
+                  welcome!
+              </h1>
+          </div>
+          <div className=
+                   "App-other"
+          >
+              <h1>This is React App -ohter.</h1>
+          </div>
+      </Fragment>
+  );
+}
+
+export default App;
